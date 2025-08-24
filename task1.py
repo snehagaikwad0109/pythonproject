c93777532c921f72ad170828c995c00dae74ff2d
snehagaikwad0109 <snehagaikwad0109@gmail.com>
2025-08-24 20:56:44 +0530

    Add files via upload
    Task 1 Take user value by using input function then convert the same into integer value and thenper form mathematica operations on the same
    
    Task 2 Take user vale by input function store the same into variables and the concatenate the variables by usin "+" operation

---
+++ task1.py
@@ -0,0 +1,9 @@
+a=input('Enter first number: ')
+a=int(a)
+b=input('Enter second number: ')
+b=int(b)
+print("Addition :", a+b)
+print("Subtraction :", a-b)
+print("Multiplication:", a*b)
+print("Division:", a/b)
+
---
+++ task2.py
@@ -0,0 +1,6 @@
+#a=print("Enter your First Name")
+#b=print("Enter your Last Name")
+a=input("Enter your First Name")
+b=input("Enter your Last Name")
+c= a+" "+ b+"!"
+print("Hello,",c,"welcome to the Python program.")
